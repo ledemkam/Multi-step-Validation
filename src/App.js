fb686cc7cbc886f300864a11e6aea9fddf9de9f6
rico kamdem <ledemkam@gmail.com>
2021-11-17 03:03:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MultiForm from './Components/MultiForm/MultiForm';
+
+const App = () => {
+   return (
+      <div className="App">
+         <MultiForm />
+      </div>
+   );
+};
+
+export default App;
